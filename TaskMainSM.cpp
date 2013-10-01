21f8d31aa9d388e5b7c6e8e964654c1c48883291
Andrew Ho <hero.andrew.ho@gmail.com>
2013-10-01 16:18:55 -0400

    Rewinded Back to Previous Commit

--- TaskMainSM.cpp
+++ TaskMainSM.cpp
@@ -50,7 +50,7 @@ void TaskMainSM::ST_Start()
 {
 	//Remove Later
 	next_state = INIT;
-	
+
 }
 
 // SM attempts to find the line
@@ -59,7 +59,7 @@ void TaskMainSM::ST_Find()
 	//Sensor Input
 	bright_l = leftLight.getBrightness(); // Left light sensor
 	bright_r = rightLight.getBrightness(); // Right light sensor
-	
+
 	switch(next_state)
 	{
 	case INIT:
@@ -86,24 +86,16 @@ void TaskMainSM::ST_Find()
 		break;
 	default:
 		PrintPlease = 0;
-<<<<<<< HEAD
 		next_state = IDLE;
-		
+
 		leftMotor.setPWM(0); // Left motor stop
 		rightMotor.setPWM(0); // Right motor stop
 		InternalEvent(ST_START);
 		next_state = INIT;
-=======
-		next_state = state_idle();
-		InternalEvent(ST_TRACK);
-		break;
-	default:
-		next_state = state_init();
->>>>>>> Initial Track and Reach Waypoint(Gray)
 		InternalEvent(ST_START);
 		break;
 	}
-	
+
 	//Jitter Mode Activate!
 	//leftMotor.setPWM(NOSPEED); // Left motor stop
 	//rightMotor.setPWM(NOSPEED); // Right motor stop
@@ -113,49 +105,11 @@ void TaskMainSM::ST_Find()
 // Follow the line
 void TaskMainSM::ST_Track()
 {
-	//editing begins 9/29
-	//Sensor Input
-	bright_l = leftLight.getBrightness(); // Left light sensor
-	bright_r = rightLight.getBrightness(); // Right light sensor
-	
-	// LOCATION C: LOOP CODE
-	switch(next_state_tt)
-	{
-	case CRUISE:
-		PrintPlease = 5;
-		next_state_tt = state_cruise();
-		break;
-	case CORNERTURNLEFT:
-		PrintPlease = 6;
-		next_state_tt = state_corner_turn_left();
-		break;
-	case CORNERTURNRIGHT:
-		PrintPlease = 7;
-		next_state_tt = state_corner_turn_right();
-		break;
-	case ALIGNGREY:
-		PrintPlease = 8;
-		next_state_tt = state_align_grey();
-		break;
-	case WAYPOINT:
-		PrintPlease = 9;
-		next_state_tt = state_waypoint();
-		break;
-	default:
-		next_state_tt = state_cruise();
-		InternalEvent(ST_TRACK);
-		break;
-	}
-	//Jitter Mode Activate!
-	//leftMotor.setPWM(NOSPEED); // Left motor stop
-	//rightMotor.setPWM(NOSPEED); // Right motor stop
 }
  
 // Sit around and wait for touch event, at waypoint
 void TaskMainSM::ST_Idle()
 {
-	bright_l = leftLight.getBrightness(); // Left light sensor
-	bright_r = rightLight.getBrightness(); // Right light sensor
 }
 
 
@@ -164,14 +118,9 @@ extern "C"
 {
 bool align(bool isLeftTrue, bool isRightTrue)
 {
-<<<<<<< HEAD
 	//Defualt return state
 	bool ret =  false;
-=======
-	//Default return state
-	state_t ret =  FWD_UNTIL_TAN;
->>>>>>> Initial Track and Reach Waypoint(Gray)
-	
+
 	//Both on line, change states
 	if(isLeftTrue && isRightTrue)
 	{
@@ -197,9 +146,9 @@ bool align(bool isLeftTrue, bool isRightTrue)
 		rightMotor.setPWM(BASESPEED); // Right motor forward
 		clock.wait(MOTORTIMESTEP); // Perform for duration of .1 seconds
 	}
-	
+
 	return ret;
-	
+
 }
 
 FindSM_states state_rotate_align(void)
@@ -207,7 +156,7 @@ FindSM_states state_rotate_align(void)
 
 	//Default return state
 	FindSM_states ret =  ROTATE_ALIGN;
-	
+
 	if(tape_flag == false)
 	{ 
 		if (isBlk(bright_l)) //when black tape first seen by left
@@ -230,171 +179,11 @@ FindSM_states state_rotate_align(void)
 		rightMotor.setPWM(BASESPEED); // Right motor backwards
 		clock.wait(MOTORTIMESTEP); // Perform for duration of .1 seconds
 	}
-	
+
 	return ret;
 
 }
 
-<<<<<<< HEAD
 /** Track SM FUnctions for now **/
-=======
-state_t state_idle(void)
-{
-	
-	leftMotor.setPWM(0); // Left motor forward
-	rightMotor.setPWM(0); // Right motor forward
-		
-	return IDLE;
-}
-/**----------------------------**/
-/** Track SM Functions for now **/
-/**----------------------------**/
-state_tt state_cruise(void)
-{
-	state_tt ret = CRUISE;
-	
-	if (isGry(bright_l) || isGry(bright_r))
-	{
-		ret = ALIGNGREY;
-	}
-	
-	else if (lfix > 4.0 || rfix >4.0)
-	{
-		if (lfix > 4.0) 			//then sharp right turn
-		{
-			leftMotor.setPWM(BASESPEED); 
-			rightMotor.setPWM(BASESPEED); 
-			ret = CORNERTURNRIGHT;
-		}
-		else if (rfix > 4.0)		//then sharp left turn
-		{
-			leftMotor.setPWM(BASESPEED); 
-			rightMotor.setPWM(BASESPEED); 
-			ret = CORNERTURNLEFT; 
-		}
-	}
-	else if (isBlk(bright_r))
-	{
-		lfix += .25;
-		if (lfix > 4.0)
-		{
-			lfix = 4.1;
-		}
-		leftMotor.setPWM(lfix*BASESPEED); // Left motor forward
-		rightMotor.setPWM(-BASESPEED); // Right motor zero
-		clock.wait(MOTORTIMESTEP); // Perform for duration of .1 seconds
-	}
-	else if (isBlk(bright_l))
-	{
-		rfix += .25;
-		if (rfix > 4.0)
-		{
-			rfix = 4.1;
-		}
-		leftMotor.setPWM(-BASESPEED); // Left motor zero
-		rightMotor.setPWM(rfix*BASESPEED); // Right motor backwards
-		clock.wait(MOTORTIMESTEP); // Perform for duration of .1 seconds
-	}
-	else
-	{
-		rfix = 1.2;
-		lfix = 1.2;
-		leftMotor.setPWM(lfix*BASESPEED); // Left motor forward
-		rightMotor.setPWM(rfix*BASESPEED); // Right motor forward
-		clock.wait(MOTORTIMESTEP); // Perform for duration of .1 seconds
-	}
 
-	return ret;
-	
 }
-
-state_tt state_corner_turn_left(void)
-{
-	state_tt ret = CORNERTURNLEFT;
-	if (isBlk(bright_l))
-	{
-		leftMotor.setPWM(-2*BASESPEED); // Left motor forward
-		rightMotor.setPWM(2*BASESPEED); // Right motor backwards
-		clock.wait(MOTORTIMESTEP); // Perform for duration of .1 seconds
-	}
-	// if (isBlk(bright_l) && isBlk(bright_r))
-	// {
-		// leftMotor.setPWM(-BASESPEED); // Left motor forward
-		// rightMotor.setPWM(3*BASESPEED); // Right motor backwards
-		// clock.wait(MOTORTIMESTEP); // Perform for duration of .1 seconds
-	// }
-	
-	else if (isWht(bright_l) && isWht(bright_r))
-	{
-		ret = CRUISE;
-	}
-	return ret;
-}
-
-state_tt state_corner_turn_right(void)
-{
-	state_tt ret = CORNERTURNRIGHT;
-	
-	if (isBlk(bright_r))
-	{
-		leftMotor.setPWM(2*BASESPEED); // Left motor forward
-		rightMotor.setPWM(-2*BASESPEED); // Right motor backwards
-		clock.wait(MOTORTIMESTEP); // Perform for duration of .1 seconds
-	}
-	// if (isBlk(bright_l) && isBlk(bright_r))
-	// {
-		// leftMotor.setPWM(3*BASESPEED); // Left motor forward
-		// rightMotor.setPWM(-BASESPEED); // Right motor backwards
-		// clock.wait(MOTORTIMESTEP); // Perform for duration of .1 seconds
-	// }
-	else if (isWht(bright_l) && isWht(bright_r))
-	{
-		ret = CRUISE;
-	}
-	return ret;
-}
-
-state_tt state_align_grey(void)
-{
-	
-	//Default return state
-	state_tt ret =  ALIGNGREY;
-	if (isGry(bright_r) && isGry(bright_l))
-	{
-		ret = WAYPOINT;
-	}
-	else if (!isGry(bright_l) && !isGry(bright_r))  //guarding against random values
-	{
-		leftMotor.setPWM(BASESPEED); // Left motor forward
-		rightMotor.setPWM(BASESPEED); // Right motor backwards
-		ret = CRUISE;
-	}
-	else if (isGry(bright_l))
-	{
-		leftMotor.setPWM(NOSPEED); // Left motor forward
-		rightMotor.setPWM(BASESPEED); // Right motor backwards
-		clock.wait(MOTORTIMESTEP); // Perform for duration of .1 seconds
-	}
-	else if (isGry(bright_r))
-	{
-		leftMotor.setPWM(BASESPEED); // Left motor forward
-		rightMotor.setPWM(NOSPEED); // Right motor backwards
-		clock.wait(MOTORTIMESTEP); // Perform for duration of .1 seconds
-	}
-	if (isGry(bright_r) && isGry(bright_l))
-	{
-		ret = WAYPOINT;
-	}
-	return ret;
-}
->>>>>>> Initial Track and Reach Waypoint(Gray)
-
-state_tt state_waypoint(void)
-{
-	leftMotor.setPWM(NOSPEED); // Left motor forward
-	rightMotor.setPWM(NOSPEED); // Right motor forward
-	clock.wait(MOTORTIMESTEP); // Perform for duration of .1 seconds
-	return WAYPOINT;
-}
-}
-
--- TaskMainSM.h
+++ TaskMainSM.h
@@ -9,7 +9,6 @@
 //From Early Imp
 extern "C" 
 {
-<<<<<<< HEAD
 
 /**Find State Declarations**/
 typedef enum{INIT, FWD_UNTIL_TAN, STEP, ROTATE_ALIGN, IDLE} FindSM_states;
@@ -17,34 +16,6 @@ bool align(bool isLeftTrue, bool isRightTrue);
 FindSM_states state_rotate_align(void);
 //Silly globasl, fix later
 FindSM_states next_state;
-=======
-typedef enum{INIT, FWD_UNTIL_TAN, STEP, ROTATE_ALIGN, IDLE} state_t;
-typedef enum{CRUISE, CORNERTURNLEFT, CORNERTURNRIGHT, ALIGNGREY, WAYPOINT} state_tt;
-
-//find functions
-state_t state_init(void);
-state_t state_fwd_until_tan(void);
-// state_t state_find(void);
-// state_t state_align(void);
-state_t state_step(void);
-state_t state_rotate_align(void);
-state_t state_idle(void);
-
-
-float rfix = 1.0;
-float lfix = 1.0;
-//track functions
-state_tt state_cruise(void);
-state_tt state_corner_turn_left(void);
-state_tt state_corner_turn_right(void);
-state_tt state_align_grey(void);
-state_tt state_waypoint(void);
-
-//Silly globals, fix later
-state_t next_state;
-state_tt next_state_tt;
-
->>>>>>> Initial Track and Reach Waypoint(Gray)
 int bright_l, bright_r;
 bool tape_flag = 0;
 
