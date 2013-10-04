4d9e17b6f8d8733fab4b4f65b0946fcac5b5e329
stephens <st.summerlin@gmail.com>
2013-10-04 14:48:31 -0400

    Made Everything Tasks

--- TaskDisplay.cpp
+++ TaskDisplay.cpp
@@ -12,7 +12,6 @@ TASK(TaskDisplay)
 {
 		// Read the sensors at every iteration
 		int dist = sonar.getDistance(); // Read distance (sonar) sensor
-		int bump = touch.isPressed();// Read touch (bump) sensor touch
 		int bright_l = leftLight.getBrightness(); // Left light sensor
 		int bright_r = rightLight.getBrightness(); // Right light sensor
 		int steer_count = steering.getCount(); // Get steering encoder
@@ -22,11 +21,10 @@ TASK(TaskDisplay)
 		lcd.clear(); // Clear the current LCD display
 		lcd.putf("sn", "NXT Sensors");
 		lcd.putf("sddn", "Light: ", bright_l,0,bright_r,5);
-		lcd.putf("sdn", "Bump: ", bump,0); // Binary
 		lcd.putf("sdn", "RMMult:", (int) (RMMult*100.0),0);
 		lcd.putf("sdn", "LMMult:",(int) (LMMult*100.0),0);
 		lcd.putf("sdn", "Sine Test:", PublixTest, 0);
-		lcd.putf("sdn", "In State = ", PrintPlease, 0); // In flowers
+		lcd.putf("sdn", "In State = ", PrintPlease, 0);
         lcd.disp(); // Update the LCD display
 	
 	TerminateTask();
--- TaskMain.cpp
+++ TaskMain.cpp
@@ -5,21 +5,18 @@ extern "C"
 {
 TASK(TaskMain)
 {	
-	// Endless loop
-	while(1)
-	{	
-		if(bump == 1 )
-		{
-			TaskMainSM_inst.Touch();
-		}
-		else
-		{
-			TaskMainSM_inst.Run();
-		}
-		
-		// Wait some time between iterations
-		clock.wait(MAIN_WAIT); //TODO: Find a better timing method than this later, shouldn't need
+	int bump = touch.isPressed(); //TODO: FInd better way to do this
+	
+	if(bump == 1 )
+	{
+		TaskMainSM_inst.Touch();
 	}
+	else
+	{
+		TaskMainSM_inst.Run();
+	}
+	
+	TerminateTask();
 	
 } // End Task
 
